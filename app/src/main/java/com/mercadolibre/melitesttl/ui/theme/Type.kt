52ecad65056254fa83c6b4bd120c01552810e952
Cristhian <criferlo@hotmail.com>
2023-02-12 16:04:59 -0500

    Adding custom themes and fonts

--- app/src/main/java/com/mercadolibre/melitesttl/ui/theme/Type.kt
+++ app/src/main/java/com/mercadolibre/melitesttl/ui/theme/Type.kt
@@ -2,33 +2,32 @@ package com.mercadolibre.melitesttl.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.mercadolibre.melitesttl.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
+val fonts = FontFamily(
+    Font(R.font.raleway_bold, FontWeight.Bold),
+    Font(R.font.raleway_regular)
+)
+
+val CustomTypography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fonts,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 12.sp,
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    titleSmall = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Bold,
+        fontSize = 10.sp,
+    ),
+    titleLarge = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp,
+    ),
+
 )
